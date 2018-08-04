97b4196243bf135cd9041413032f3304d7309754
vishalsero <42085997+vishalsero@users.noreply.github.com>
2018-08-04 12:51:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
+	echo $json;
+	exit();
 	$text = $json->result->parameters->text;
 
 	switch ($text) {
