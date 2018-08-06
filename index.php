3132935aec83fb8beafcfabf17e9992782a6864f
vishalsero <42085997+vishalsero@users.noreply.github.com>
2018-08-06 11:44:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->resolvedQuery;
+	$text = $json->result->parameters->text;
 
 	switch ($text) {
 		case 'hi':
