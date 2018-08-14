bb94a21c6b3d3702b1b28c326ae31bbecd3b5421
vishalsero <42085997+vishalsero@users.noreply.github.com>
2018-08-14 17:31:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -54,13 +54,14 @@ Kindly find the list of the modules below-
 - Dashboards & Reports";
 	               
 	break;
-		
-		default:
-			$speech = "About Academia ERP-
+		case 'About ERP':
+		$speech = "About Academia ERP-
 Academia by Serosoft is an award-winning Student Information System powering over 200 institutions across the globe. The robust, feature-rich, analytics-equipped, user-friendly Academia - built on a cutting-edge and flexible architecture - enables educational institutions to automate & streamline their functions and processes for both learning and administration, from prospecting to graduation. It boasts of tremendous capability to handle multi-center operations as well.
 There are competing educational software available on the market which may seem similar to our solution, but the majority of these are web-based applications with limited functionality, reporting, and non-scalable in future. In addition, they all require extra customization in the future to map with changing client processes. Moreover, there are problems with software integration with their system and adaptability to their requirements.
 Academia ERP is a comprehensive, scalable, highly reliable, secure and built on the latest technology stack to manage every aspect of the education process and deliver the best possible user experience to educational institutions. Academia is highly configurable, scalable, feature rich, mobile ready and comes with extensive reporting capability from the start to serve the needs of a top-class Universities/Colleges/Schools. It will make processes efficient, optimize your resources, reduce your operational risk and focus on teacher & student ";
 			break;
+		default:
+			$speech = "Sorry, I didn't get that. Can you rephrase?"
 	}
 
 	
